dc7953e96334bacf92304706dbf41b28e81a8b44
Kimbomyeong <kbm41890507@gmail.com>
2024-12-26 11:01:32 +0900

    my/profile

--- src/main/java/com/iteamoa/mypage/controller/UserProfileController.java
+++ src/main/java/com/iteamoa/mypage/controller/UserProfileController.java
@@ -17,7 +17,7 @@ import com.iteamoa.mypage.service.S3Service;
 import com.iteamoa.mypage.service.UserProfileService;
 
 @RestController
-@RequestMapping("/profile")
+@RequestMapping("/my/profile")
 public class UserProfileController {
     private final UserProfileService userProfileService;
     private final S3Service s3Service;
