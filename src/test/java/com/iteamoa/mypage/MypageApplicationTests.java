3a56b8da15f0a8b0fb57799dba6a8172ace74cfc
Brynn Park <pbh7080@gmail.com>
2024-12-29 22:26:14 +0900

    del test

--- src/test/java/com/iteamoa/mypage/MypageApplicationTests.java
+++ src/test/java/com/iteamoa/mypage/MypageApplicationTests.java
@@ -2,12 +2,3 @@ package com.iteamoa.mypage;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MypageApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
